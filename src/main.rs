8b19dd9f07cb18f4219dc10fa9970b6c26888a9c
Felix Nilles <felix@dienilles.de>
2023-12-03 11:00:23 +0100

    updated to use reqwest

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,11 @@
 use std::time::Duration;
 use leptos::*;
-use wasm_bindgen::prelude::*;
-use wasm_bindgen_futures::JsFuture;
-use web_sys::{RequestInit, RequestMode, RequestCache, Request, Response};
+
 mod trains;
-mod cal;
 mod mensa;
 
 fn main() {
-    spawn_local(async move {
-        cal::memes().await;
-    });
     leptos::mount_to_body(move || view! { 
-
-        <div style="height:100vh; width:80vw">
             <div style="height:5vh; width:100%">
                 <Notification_Bar/>
             </div>
@@ -23,10 +15,6 @@ fn main() {
             <div style="height:20vh; width:100%;">
               <mensa::App2/>
             </div>
-        </div>
-        <div style="height:100vh; width:20vw">
-            <cal::App/>
-        </div>
     })
 
 }
@@ -50,17 +38,3 @@ fn Notification_Bar() -> impl IntoView {
     }
 }
 
-#[wasm_bindgen]
-pub async fn fetch(url:String) -> String{
-    let mut opts = RequestInit::new();
-    opts.method("GET");
-    opts.mode(RequestMode::Cors);
-    opts.cache(RequestCache::NoCache);
-    let request = Request::new_with_str_and_init(&url, &opts).unwrap();
-    let window = web_sys::window().unwrap();
-    let resp_value = JsFuture::from(window.fetch_with_request(&request)).await.unwrap();
-    let resp: Response = resp_value.dyn_into().unwrap();
-    let text = JsFuture::from(resp.text().unwrap()).await.unwrap();
-    let text = text.as_string().unwrap();
-    text
-}
