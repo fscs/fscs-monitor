33e6e64eec94d91b0ece74e05bc12242ec4f4903
Felix Nilles <felixnilles@MacBook-Pro-von-Felix.local>
2023-12-06 09:21:54 +0100

    added scroll back to the trains

--- src/trains.rs
+++ src/trains.rs
@@ -1,6 +1,6 @@
-use std::time::Duration;
 use leptos::*;
 use serde_json::Value;
+use std::time::Duration;
 use wasm_bindgen::prelude::*;
 #[component]
 pub fn App() -> impl IntoView {
@@ -21,17 +21,20 @@ pub fn App() -> impl IntoView {
 }
 
 #[component]
-fn Station(id:String) -> impl IntoView {
-   
-    let (state,set_state) = create_signal(vec![vec![String::new();10]; 10]);
+fn Station(id: String) -> impl IntoView {
+    let (state, set_state) = create_signal(vec![vec![String::new(); 10]; 10]);
     let (name, set_name) = create_signal(String::new());
-    let id2 = id.clone(); 
+    let id2 = id.clone();
 
     spawn_local(async move {
         let list = list(id2.clone());
         let list = list.await.unwrap();
         list.split("\n").for_each(|_y| {
-            set_state.set(list.split("\n").map(|x| x.split(" && ").map(|x| x.to_string()).collect::<Vec<_>>()).collect::<Vec<_>>());
+            set_state.set(
+                list.split("\n")
+                    .map(|x| x.split(" && ").map(|x| x.to_string()).collect::<Vec<_>>())
+                    .collect::<Vec<_>>(),
+            );
         });
         let name = get_station_name(id2.clone());
         let name = name.await.unwrap();
@@ -45,13 +48,16 @@ fn Station(id:String) -> impl IntoView {
                 let list = list(id.clone());
                 let list = list.await.unwrap();
                 list.split("\n").for_each(|_y| {
-                    set_state.set(list.split("\n").map(|x| x.split(" && ").map(|x| x.to_string()).collect::<Vec<_>>()).collect::<Vec<_>>());
+                    set_state.set(
+                        list.split("\n")
+                            .map(|x| x.split(" && ").map(|x| x.to_string()).collect::<Vec<_>>())
+                            .collect::<Vec<_>>(),
+                    );
                 });
                 let name = get_station_name(id.clone());
                 let name = name.await.unwrap();
-                
-                set_name.set(name);
 
+                set_name.set(name);
             });
         },
         Duration::from_secs(60),
@@ -67,7 +73,17 @@ fn Station(id:String) -> impl IntoView {
                          <tr class="hidden">
                          </tr>
                      };
-                 }else{
+                 }else if x[1].len() >= 27{
+                     return view! {
+                         <tr>
+                             <th style="text-align:left;">{x[0].clone()}</th>
+                             <th style="text-align:left; line-height:1; max-width:25vw; overflow:hidden;"><div class="scroll" style="color:#00cc00; width:auto;"><span>{x[1].clone()}</span><span>{x[1].clone()}</span><span>{x[1].clone()}</span></div></th>
+                             <th style="text-align:right;">{x[2].clone()}</th>
+
+                         </tr>
+                        }
+                     }
+                 else {
                      return view! {
                          <tr>
                              <th style="text-align:left;">{x[0].clone()}</th>
@@ -75,24 +91,22 @@ fn Station(id:String) -> impl IntoView {
                              <th style="text-align:right;">{x[2].clone()}</th>
 
                          </tr>
-                        }
                      }
+                 }
              }).collect::<Vec<_>>()
             }
             </table>
         </div>
-    }
+    };
 }
 
 #[wasm_bindgen]
-pub async fn get_departures(id:String, limit:i32) -> Result<JsValue, JsValue> {
-
-    let url = format!("https://app.vrr.de/vrrstd/XML_DM_REQUEST?outputFormat=JSON&commonMacro=dm&type_dm=any&name_dm={}&language=de&realtime=1&lsShowTrainsExplicit=1&mode=direct&typeInfo_dm=stopID&limit={}", id, limit); 
+pub async fn get_departures(id: String, limit: i32) -> Result<JsValue, JsValue> {
+    let url = format!("https://app.vrr.de/vrrstd/XML_DM_REQUEST?outputFormat=JSON&commonMacro=dm&type_dm=any&name_dm={}&language=de&realtime=1&lsShowTrainsExplicit=1&mode=direct&typeInfo_dm=stopID&limit={}", id, limit);
 
     let text = reqwest::get(url).await.unwrap().text().await.unwrap();
 
     Ok(JsValue::from_str(&text))
-
 }
 
 struct Train {
@@ -103,27 +117,36 @@ struct Train {
 }
 
 #[wasm_bindgen]
-pub async fn list(id:String) -> Result<String, JsValue> {
+pub async fn list(id: String) -> Result<String, JsValue> {
     let mut vec = Vec::new();
     let mut limit = 40;
     if id.clone() == "20018249" {
         limit = 100;
     }
 
-    let json: Value = serde_json::from_str(&get_departures(id.clone(), limit).await.unwrap().as_string().unwrap()).unwrap();
+    let json: Value = serde_json::from_str(
+        &get_departures(id.clone(), limit)
+            .await
+            .unwrap()
+            .as_string()
+            .unwrap(),
+    )
+    .unwrap();
 
     let mut x = 1;
-    while vec.len() < 9{
+    while vec.len() < 9 {
         let train = get_traindata(json.clone(), x as usize);
-        
+
         let string = format!("{} && {} && {}min", train.line, train.direction, train.time);
-        
+
         if train.time.parse::<i32>().unwrap() >= 3 {
             if id.clone() == "20018249" {
-                if ((train.train_type == "S-Bahn") || (train.train_type == "Regionalzug"))&&(train.time.parse::<i32>().unwrap() > 15) {
+                if ((train.train_type == "S-Bahn") || (train.train_type == "Regionalzug"))
+                    && (train.time.parse::<i32>().unwrap() > 15)
+                {
                     vec.push(string);
                 }
-            }else{ 
+            } else {
                 if !train.direction.contains("Uni") {
                     vec.push(string);
                 }
@@ -136,25 +159,31 @@ pub async fn list(id:String) -> Result<String, JsValue> {
 }
 
 #[wasm_bindgen]
-pub async fn get_station_name(id:String) -> Result<String, JsValue> {
-
-    let raw = get_departures(id,2).await.unwrap().as_string().unwrap();
+pub async fn get_station_name(id: String) -> Result<String, JsValue> {
+    let raw = get_departures(id, 2).await.unwrap().as_string().unwrap();
 
     let json: Value = serde_json::from_str(&raw).unwrap();
 
-    let name = json["departureList"][0]["stopName"].to_string().replace("\"", "");
+    let name = json["departureList"][0]["stopName"]
+        .to_string()
+        .replace("\"", "");
     Ok(name)
-
 }
 
-fn get_traindata(json:Value, id:usize) -> Train {
-    let day_train = json["departureList"][&id]["dateTime"]["day"].to_string().replace("\"", "");
+fn get_traindata(json: Value, id: usize) -> Train {
+    let day_train = json["departureList"][&id]["dateTime"]["day"]
+        .to_string()
+        .replace("\"", "");
     let day_now = chrono::Local::now().format("%d").to_string();
 
-    let hour_train = json["departureList"][&id]["dateTime"]["hour"].to_string().replace("\"", ""); 
+    let hour_train = json["departureList"][&id]["dateTime"]["hour"]
+        .to_string()
+        .replace("\"", "");
     let hour_now = chrono::Local::now().format("%H").to_string();
 
-    let minute_train = json["departureList"][&id]["dateTime"]["minute"].to_string().replace("\"", "");
+    let minute_train = json["departureList"][&id]["dateTime"]["minute"]
+        .to_string()
+        .replace("\"", "");
     let minute_now = chrono::Local::now().format("%M").to_string();
 
     let hour_train = hour_train.parse::<i32>().unwrap();
@@ -171,15 +200,23 @@ fn get_traindata(json:Value, id:usize) -> Train {
     let diff;
     if day_off == 1 {
         diff = 1440 - (hour_now * 60 + minute_now) + (hour_train * 60 + minute_train);
-    }else {
+    } else {
         diff = hour_off * 60 + minute_off;
     }
     _times = diff.to_string();
 
     Train {
-        line: json["departureList"][&id]["servingLine"]["number"].to_string().replace("\"", "").replace(" (RRX)", ""),
-        direction: json["departureList"][&id]["servingLine"]["direction"].to_string().replace("\"", ""),
+        line: json["departureList"][&id]["servingLine"]["number"]
+            .to_string()
+            .replace("\"", "")
+            .replace(" (RRX)", ""),
+        direction: json["departureList"][&id]["servingLine"]["direction"]
+            .to_string()
+            .replace("\"", "")
+            +" ",
         time: _times,
-        train_type: json["departureList"][&id]["servingLine"]["name"].to_string().replace("\"", ""),
+        train_type: json["departureList"][&id]["servingLine"]["name"]
+            .to_string()
+            .replace("\"", ""),
     }
 }
