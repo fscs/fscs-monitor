49be3ce00186bd4854225089bf642361e46bd393
Felix Nilles <felix@dienilles.de>
2023-12-08 19:53:22 +0100

    fix

--- src/cal.rs
+++ src/cal.rs
@@ -1,9 +1,9 @@
-use std::time::Duration;
 use chrono::DateTime;
 use leptos::{
     component, create_signal, leptos_dom::logging::console_log, set_interval, view, IntoView,
     SignalGet, SignalSet,
 };
+use std::time::Duration;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen_futures::spawn_local;
 
@@ -18,7 +18,9 @@ struct Event {
 pub async fn memes() -> String {
     let mut vec = vec![Event {
         title: String::new(),
-        start: DateTime::parse_from_rfc3339("2021-01-01T00:00:00Z").unwrap().into(),
+        start: DateTime::parse_from_rfc3339("2021-01-01T00:00:00Z")
+            .unwrap()
+            .into(),
         location: String::new(),
         description: String::new(),
     }];
@@ -37,7 +39,9 @@ pub async fn memes() -> String {
         let mut event = Event {
             title: String::new(),
             location: String::new(),
-            start: DateTime::parse_from_rfc3339("2021-01-01T00:00:00Z").unwrap().into(),
+            start: DateTime::parse_from_rfc3339("2021-01-01T00:00:00Z")
+                .unwrap()
+                .into(),
             description: String::new(),
         };
 
@@ -53,14 +57,46 @@ pub async fn memes() -> String {
             );
         }
 
-        if i.contains("DTSTART") {
+        if i.contains("DTSTART;TZID=Europe/Berlin:") {
             console_log("test");
-            let date = i.split("DTSTART").collect::<Vec<_>>()[1]
+            let date = i.split("DTSTART;TZID=Europe/Berlin:").collect::<Vec<_>>()[1]
                 .split("\n")
                 .collect::<Vec<_>>()[0]
                 .to_string();
-            event.start = DateTime::parse_from_rfc3339(&date).unwrap().into();
             console_log(&date);
+            //parse Date to DateTime 20230101T000000
+            let date = format!("{}T{}Z", &date[0..8], &date[9..15]);
+            let date = format!(
+                "{}{}{}{}{}{}{}",
+                &date[0..4],
+                "-",
+                &date[4..6],
+                "-",
+                &date[6..8],
+                "T",
+                &date[9..15]
+            );
+            let date = format!(
+                "{}{}{}{}{}{}{}",
+                &date[0..11],
+                &date[11..13],
+                ":",
+                &date[13..15],
+                ":",
+                "00",
+                "Z"
+            );
+
+            console_log(&date);
+
+            event.start = DateTime::parse_from_rfc3339(&date).unwrap().into();
+            console_log(
+                &date
+                    .parse::<DateTime<chrono::Utc>>()
+                    .unwrap()
+                    .format("%d.%m.%Y %H:%M")
+                    .to_string(),
+            );
         }
 
         event.location = "TBA".to_string();
@@ -103,7 +139,6 @@ pub async fn memes() -> String {
 
     vec.sort_by(|a, b| a.start.cmp(&b.start));
 
-
     //format Date to string
 
     let mut string = String::new();
@@ -111,23 +146,21 @@ pub async fn memes() -> String {
     string = string
         + &vec[1].title
         + " && "
-        + &vec[1].start.format("%d").to_string()
+        + &vec[1].start.format("%d.%m.%Y %H:%M").to_string()
         + " && "
         + &vec[1].location
         + " && "
         + &vec[1].description
         + "\n";
-
     console_log("test");
     console_log(&string.clone());
-
 
     for i in 2..vec.len() {
         if vec[i].title != vec[i - 1].title {
             string = string
                 + &vec[i].title
                 + " && "
-                + &vec[i].start.format("%d").to_string()
+                + &vec[i].start.format("%d.%m.%Y %H:%M").to_string()
                 + " && "
                 + &vec[i].location
                 + " && "
@@ -186,84 +219,76 @@ pub fn App() -> impl IntoView {
 
         <div style="width:100%; height:100%">
             <ul style="list-style-type:none;padding-left:0px">
-        {move || events.get().iter().map(move |x| {
-                if x[0].is_empty() {
-                    return view! {
-                        <li class="hidden" style="width:100%">
+            {move || events.get().iter().map(move |x| {
+                                                          if x[0].is_empty() {
+                                                              return view! {
+                                                                  <li class="hidden" style="width:100%">
 
-                        </li>
-                        <li>
-                        </li>
-                    };
-                }else{
-                    if x[6].len() > 17 {
-                        if x[0].len() > 20 {
-                            return view! {
-                                <li style="width:100%; font-size:1.8vw; color: #00cc00; padding-bottom:0px">
-                                {x[1].clone()}.{x[2].clone()}.{x[3].clone()}
-                                " "
-                                {x[4].clone()}:{x[5].clone()}
-                                </li>
-                                <li style="width:100%; font-size:1.8vw;overflow:hidden; padding-bottom:10px">
+                                                                      </li>
+                                                                      <li>
+                                                                      </li>
+                                                              };
+                                                          }else{
+                                                              if x[2].len() > 17 {
+                                                                  if x[0].len() > 20 {
+                                                                      return view! {
+                                                                          <li style="width:100%; font-size:1.8vw; color: #00cc00; padding-bottom:0px">
+                                                                          {x[1].clone()}
+                                                                          </li>
+                                                                              <li style="width:100%; font-size:1.8vw;overflow:hidden; padding-bottom:10px">
 
-                                <div style="width:fit-content; overflow:hidden" class="scroll"><span>{x[0].clone()+" "}</span><span>{x[0].clone()+" "}</span><span>{x[0].clone()+" "}</span></div>
-                                </li><li style="padding-bottom:30px; font-size:1.3vw">
-                                {x[6].clone()}
-                                </li>
+                                                                              <div style="width:fit-content; overflow:hidden" class="scroll"><span>{x[0].clone()+" "}</span><span>{x[0].clone()+" "}</span><span>{x[0].clone()+" "}</span></div>
+                                                                              </li><li style="padding-bottom:30px; font-size:1.3vw">
+                                                                              {x[2].clone()}
+                                                                          </li>
 
-                            };
-                        }
-                        return view! {
-                            <li style="width:100%; font-size:1.8vw; color: #00cc00; padding-bottom:0px">
-                            {x[1].clone()}.{x[2].clone()}.{x[3].clone()}
-                            " "
-                            {x[4].clone()}:{x[5].clone()}
-                            </li>
-                            <li style="width:100%; font-size:1.8vw;padding-bottom:10px">
+                                                                      };
+                                                                  }
+                                                                  return view! {
+                                                                      <li style="width:100%; font-size:1.8vw; color: #00cc00; padding-bottom:0px">
+                                                                      {x[1].clone()}
+                                                                      </li>
+                                                                          <li style="width:100%; font-size:1.8vw;padding-bottom:10px">
 
-                            {x[0].clone()}
-                            </li><li style="padding-bottom:30px; font-size:1.3vw">
-                                siehe Kalender
-                            </li>
+                                                                          {x[0].clone()}
+                                                                      </li><li style="padding-bottom:30px; font-size:1.3vw">
+                                                                          siehe Kalender
+                                                                          </li>
 
-                        };
-                    }
-                    if x[0].len() > 20 {
-                        return view! {
-                            <li style="width:100%; font-size:1.8vw; color: #00cc00; padding-bottom:0px">
-                            {x[1].clone()}.{x[2].clone()}.{x[3].clone()}
-                            " "
-                            {x[4].clone()}:{x[5].clone()}
-                            </li>
-                            <li style="width:100%; font-size:1.8vw;overflow:hidden; padding-bottom:10px">
+                                                                  };
+                                                              }
+                                                              if x[0].len() > 20 {
+                                                                  return view! {
+                                                                      <li style="width:100%; font-size:1.8vw; color: #00cc00; padding-bottom:0px">
+                                                                      {x[1].clone()}
+                                                                      </li>
+                                                                          <li style="width:100%; font-size:1.8vw;overflow:hidden; padding-bottom:10px">
 
-                            <div style="width:fit-content; overflow:hidden" class="scroll"><span>{x[0].clone()+" "}</span><span>{x[0].clone()+" "}</span><span>{x[0].clone()+" "}</span></div>
-                            </li><li style="padding-bottom:30px; font-size:1.3vw">
-                            {x[6].clone()}
-                            </li>
+                                                                          <div style="width:fit-content; overflow:hidden" class="scroll"><span>{x[0].clone()+" "}</span><span>{x[0].clone()+" "}</span><span>{x[0].clone()+" "}</span></div>
+                                                                          </li><li style="padding-bottom:30px; font-size:1.3vw">
+                                                                          {x[2].clone()}
+                                                                      </li>
 
-                        };
-                    }
-                    return view! {
-                        <li style="width:100%; font-size:1.8vw; color: #00cc00; padding-bottom:0px">
-                        {x[1].clone()}.{x[2].clone()}.{x[3].clone()}
-                        " "
-                        {x[4].clone()}:{x[5].clone()}
-                        </li>
-                        <li style="width:100%; font-size:1.8vw;padding-bottom:10px">
+                                                                  };
+                                                              }
+                                                              return view! {
+                                                                  <li style="width:100%; font-size:1.8vw; color: #00cc00; padding-bottom:0px">
+                                                                  {x[1].clone()}
+                                                                  </li>
+                                                                      <li style="width:100%; font-size:1.8vw;padding-bottom:10px">
 
-                        {x[0].clone()}
-                        </li><li style="padding-bottom:30px; font-size:1.3vw">
-                        {x[6].clone()}
-                        </li>
+                                                                      {x[0].clone()}
+                                                                  </li><li style="padding-bottom:30px; font-size:1.3vw">
+                                                                  {x[2].clone()}
+                                                                  </li>
 
-                    };
-                }
+                                                              };
+                                                          }
 
-            }
-        ).collect::<Vec<_>>()}
+                                                      }
+            ).collect::<Vec<_>>()}
         </ul>
-        </div>
+            </div>
 
     }
 }
